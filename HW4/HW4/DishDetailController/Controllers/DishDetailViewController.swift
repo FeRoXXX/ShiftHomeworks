6f6050fc265cde55687fb43db56dc9b583edad93
Александр Федоткин <ben115899@gmail.com>
2024-05-07 22:26:03 +0700

    Добавил делегат и методы для работы с view

--- HW4/HW4/DishDetailController/Controllers/DishDetailViewController.swift
+++ HW4/HW4/DishDetailController/Controllers/DishDetailViewController.swift
@@ -9,9 +9,41 @@ import UIKit
 
 class DishDetailViewController: UIViewController {
 
+    private lazy var dishDetailView = DishDetailView(dataManager: dataManager, numberOfItem: numberOfItem, delegate: self)
+    let dataManager: IDishesDataManager
+    let numberOfItem: Int
+    
+    init(dataManager: IDishesDataManager, numberOfItem: Int) {
+        self.dataManager = dataManager
+        self.numberOfItem = numberOfItem
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setupUI()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(true, animated: true)
     }
 
+}
+
+private extension DishDetailViewController {
+    func setupUI() {
+        view = dishDetailView
+    }
+}
+
+extension DishDetailViewController: ShowReceiptDelegate {
+    func showReceiptViewController(_ viewController: UIViewController) {
+        present(viewController, animated: true)
+    }
 }
---
+++ HW4/HW4/DishDetailController/Support/ShowReceiptDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  ShowReceiptDelegate.swift
+//  HW4
+//
+//  Created by Александр Федоткин on 07.05.2024.
+//
+
+import UIKit
+
+protocol ShowReceiptDelegate: NSObject {
+    func showReceiptViewController(_ viewController: UIViewController)
+}
