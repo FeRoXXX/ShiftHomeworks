0223d285bad010f59399763af8be0e4cf0cd8310
Александр Федоткин <ben115899@gmail.com>
2024-05-07 13:54:54 +0700

    Инициаллизировал коллекцию + элементы ячейки

--- HW4/HW4/HomeController/Views/HomeCollectionViewCell.swift
+++ HW4/HW4/HomeController/Views/HomeCollectionViewCell.swift
@@ -9,8 +9,65 @@ import UIKit
 
 class HomeCollectionViewCell: UICollectionViewCell {
     
+    private lazy var contentStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [dishImageView, dishNameLabel, horizontalContentStackView])
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.distribution = .equalCentering
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var dishNameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.textColor = .white
+        return label
+    }()
+    
+    private lazy var cookieTimeImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var cookieTimeLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var horizontalContentStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [cookieTimeImageView, cookieTimeLabel])
+        stackView.axis = .horizontal
+        stackView.distribution = .equalCentering
+        stackView.alignment = .center
+        stackView.spacing = 5
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var dishImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 20
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupUI()
+    }
+    
+    func setupData(with model: DishesModel) {
+        dishImageView.image = UIImage(named: model.dishImage)
+        dishNameLabel.text = model.dishName
+        cookieTimeImageView.image = UIImage(named: model.cookingTimeImage.rawValue)
+        cookieTimeLabel.text = model.cookingTime
     }
     
     required init?(coder: NSCoder) {
@@ -20,15 +77,24 @@ class HomeCollectionViewCell: UICollectionViewCell {
 
 private extension HomeCollectionViewCell {
     func setupUI() {
-        
+        backgroundColor = .clear
+        addSubviews()
+        setupLayout()
     }
     
     func addSubviews() {
-        
+        addSubview(contentStackView)
     }
     
     func setupLayout() {
+        let marginGuide = contentView.layoutMarginsGuide
         
+        NSLayoutConstraint.activate([
+            contentStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            contentStackView.leadingAnchor.constraint(equalTo: marginGuide.leadingAnchor),
+            contentStackView.trailingAnchor.constraint(equalTo: marginGuide.trailingAnchor),
+            contentStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -20)
+        ])
     }
 }
 
--- HW4/HW4/HomeController/Views/HomeView.swift
+++ HW4/HW4/HomeController/Views/HomeView.swift
@@ -12,9 +12,11 @@ class HomeView: UIView {
     let dataSource = HomeCollectionViewDataSource()
     
     lazy var collectionView: UICollectionView = {
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createCompositionalLayout())
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createCompositionalLayoutForPortrait())
         collectionView.delegate = delegate
         collectionView.dataSource = dataSource
+        collectionView.backgroundColor = .clear
+        collectionView.register(HomeCollectionViewCell.self, forCellWithReuseIdentifier: HomeCollectionViewCell.identifier)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         return collectionView
     }()
@@ -25,6 +27,17 @@ class HomeView: UIView {
         setupUI()
     }
     
+    override func layoutSubviews() {
+        switch UIDevice.current.orientation {
+        case .portrait, .portraitUpsideDown:
+            collectionView.collectionViewLayout = createCompositionalLayoutForPortrait()
+        case .landscapeLeft, .landscapeRight:
+            collectionView.collectionViewLayout = createCompositionalLayoutForRight()
+        default:
+            break
+        }
+    }
+    
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("view doesn't exist")
@@ -33,11 +46,27 @@ class HomeView: UIView {
 
 private extension HomeView {
     
-    func createCompositionalLayout() -> UICollectionViewLayout {
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1.0))
+    func createCompositionalLayoutForPortrait() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.3))
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.5))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        group.interItemSpacing = .fixed(10)
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 10
+        
+        section.contentInsets = .init(top: 0, leading: 16, bottom: 0, trailing: 16)
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        return layout
+    }
+    
+    func createCompositionalLayoutForRight() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.3), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.7))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item, item])
         group.interItemSpacing = .fixed(10)
         let section = NSCollectionLayoutSection(group: group)
         section.interGroupSpacing = 10
@@ -50,6 +79,7 @@ private extension HomeView {
     }
     
     func setupUI() {
+        backgroundColor = .darkGray
         setupSubviews()
         setupLayout()
     }
