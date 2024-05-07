7b17e21057ac5cba609b4cbcdff39e6d2edafa6c
Александр Федоткин <ben115899@gmail.com>
2024-05-07 22:27:02 +0700

    Создал пустой контроллер для работы с рецептами

---
+++ HW4/HW4/DishReceiptController/Controllers/DishReceiptViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DishReceiptViewController.swift
+//  HW4
+//
+//  Created by Александр Федоткин on 07.05.2024.
+//
+
+import UIKit
+
+class DishReceiptViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ HW4/HW4/DishReceiptController/Views/DishReceiptView.swift
@@ -0,0 +1,8 @@
+//
+//  DishReceiptView.swift
+//  HW4
+//
+//  Created by Александр Федоткин on 07.05.2024.
+//
+
+import Foundation
