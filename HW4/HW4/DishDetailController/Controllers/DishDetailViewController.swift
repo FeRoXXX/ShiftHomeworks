1d42f27dd513befa5e382f0a94db2332fae186f2
Александр Федоткин <ben115899@gmail.com>
2024-05-07 13:52:31 +0700

    Добавил контроллер для деталей ячейки

---
+++ HW4/HW4/DishDetailController/Controllers/DishDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DishDetailViewController.swift
+//  HW4
+//
+//  Created by Александр Федоткин on 07.05.2024.
+//
+
+import UIKit
+
+class DishDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
