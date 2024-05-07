edf6a3630fdd3f300cb19d273f0988946094d3fe
Александр Федоткин <ben115899@gmail.com>
2024-05-07 22:24:58 +0700

    Добавил Navigation Controller

--- HW4/HW4/App/SceneDelegate.swift
+++ HW4/HW4/App/SceneDelegate.swift
@@ -16,7 +16,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         window.makeKeyAndVisible()
-        window.rootViewController = HomeViewController()
+        let navigationController = UINavigationController(rootViewController: HomeViewController())
+        navigationController.setNavigationBarHidden(true, animated: true)
+        window.rootViewController = navigationController
         self.window = window
     }
 }
