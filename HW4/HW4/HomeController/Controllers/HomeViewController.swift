1c770034b689df424ede92b7083c781addac27c4
Александр Федоткин <ben115899@gmail.com>
2024-05-11 21:06:23 +0700

    Добавил обработку ошибки

--- HW4/HW4/HomeController/Controllers/HomeViewController.swift
+++ HW4/HW4/HomeController/Controllers/HomeViewController.swift
@@ -8,20 +8,20 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-    
     private lazy var homeView = HomeView(delegate: self)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupUI()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: true)
     }
 }
@@ -34,7 +34,9 @@ private extension HomeViewController {
 
 extension HomeViewController: HomeCollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let controller = DishDetailViewController(dataManager: homeView.dataSource.dishesDataManager.getDescriptionManager(from: indexPath.row))
-        navigationController?.pushViewController(controller, animated: true)
+        if let data = homeView.dataSource.dishesDataManager.getDescriptionData(from: indexPath.row) {
+            let controller = DishDetailViewController(data: data)
+            navigationController?.pushViewController(controller, animated: true)
+        }
     }
 }
