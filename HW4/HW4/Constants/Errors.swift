9773b7a145ac43f2547357042b03934e90d4fc43
Александр Федоткин <ben115899@gmail.com>
2024-05-11 20:58:42 +0700

    Добавил перечисление для ошибок

---
+++ HW4/HW4/Constants/Errors.swift
@@ -0,0 +1,14 @@
+//
+//  Errors.swift
+//  HW4
+//
+//  Created by Александр Федоткин on 11.05.2024.
+//
+
+import Foundation
+
+enum Errors: String {
+    case dataIsMissing = "Data is missing"
+    case outOfBounds = "Index out of bounds"
+    case badURL = "Bad URL"
+}
