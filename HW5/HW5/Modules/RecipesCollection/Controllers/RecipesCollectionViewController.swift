d0f7b1591995d11fdc8a43dd0f89301d238148ea
Александр Федоткин <ben115899@gmail.com>
2024-05-19 15:26:01 +0700

    Создал пустой проект

---
+++ HW5/HW5/Modules/RecipesCollection/Controllers/RecipesCollectionViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RecipesCollectionViewController.swift
+//  HW5
+//
+//  Created by Александр Федоткин on 19.05.2024.
+//
+
+import UIKit
+
+class RecipesCollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+}
+
+private extension RecipesCollectionViewController {
+    func setupUI() {
+        view.backgroundColor = .red
+    }
+}
