459817dacbc79ce423277d756706eec3b701efbb
Александр Федоткин <ben115899@gmail.com>
2024-05-19 17:36:20 +0700

    Добавил картинки + константы

---
+++ HW5/HW5/Constants/Errors.swift
@@ -0,0 +1,14 @@
+//
+//  Errors.swift
+//  HW5
+//
+//  Created by Александр Федоткин on 19.05.2024.
+//
+
+import Foundation
+
+enum Errors: String {
+    case dataIsMissing = "Data is missing"
+    case outOfBounds = "Index out of bounds"
+    case badURL = "Bad URL"
+}
---
+++ HW5/HW5/Constants/Fonts.swift
@@ -0,0 +1,13 @@
+//
+//  Fonts.swift
+//  HW5
+//
+//  Created by Александр Федоткин on 19.05.2024.
+//
+
+import UIKit
+
+enum Fonts {
+    static let systemWhite20 : [NSAttributedString.Key : Any] = [.font : UIFont.systemFont(ofSize: 20), .foregroundColor : UIColor.white]
+    static let systemWhite14 : [NSAttributedString.Key : Any] = [.font : UIFont.systemFont(ofSize: 14), .foregroundColor : UIColor.white]
+}
