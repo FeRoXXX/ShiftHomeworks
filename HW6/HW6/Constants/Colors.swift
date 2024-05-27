00c70ac35000c889fcfd9c3c2db48a570d0e1c2b
Александр Федоткин <ben115899@gmail.com>
2024-05-27 22:57:25 +0700

    Добавил константы

---
+++ HW6/HW6/Constants/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  HW6
+//
+//  Created by Александр Федоткин on 27.05.2024.
+//
+
+import UIKit
+
+enum Colors {
+    static let backgroundColor = UIColor.white
+}
+
---
+++ HW6/HW6/Constants/Errors.swift
@@ -0,0 +1,12 @@
+//
+//  Errors.swift
+//  HW6
+//
+//  Created by Александр Федоткин on 27.05.2024.
+//
+
+enum Errors: String {
+    case dataIsMissing = "Data is missing"
+    case outOfBounds = "Index out of bounds"
+    case badURL = "Bad URL"
+}
