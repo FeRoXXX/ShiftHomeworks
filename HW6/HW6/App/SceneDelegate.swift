2688e10ef46ef52641900a11da0054cc3a8aa828
Александр Федоткин <ben115899@gmail.com>
2024-05-27 22:59:02 +0700

    Инициаллизировал новый экран

--- HW6/HW6/App/SceneDelegate.swift
+++ HW6/HW6/App/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         window.makeKeyAndVisible()
-        window.rootViewController = UINavigationController(rootViewController: ViewController())
+        window.rootViewController = UINavigationController(rootViewController: ChooseScreenAssembly.build())
         self.window = window
     }
 }
