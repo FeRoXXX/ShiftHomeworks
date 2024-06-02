ede16522be7e4c58bfac650271e26da6d8c3d165
Александр Федоткин <ben115899@gmail.com>
2024-06-02 20:51:46 +0700

    Изменил дизайн проекта

--- HW7/HW7/Modules/ListOfImage/View/ListOfImageViewController.swift
+++ HW7/HW7/Modules/ListOfImage/View/ListOfImageViewController.swift
@@ -9,8 +9,35 @@ import UIKit
 
 class ListOfImageViewController: UIViewController {
     
+    private lazy var componentView = ListOfImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupUI()
+        setupClousure()
+    }
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
+
+private extension ListOfImageViewController {
+    
+    func setupUI() {
+        view = componentView
+    }
+    
+    func setupClousure() {
+        componentView.findImage = { [weak self] text in
+            
+        }
+    }
+}
+
+
