9fdf3203ce2917017612414e6a798dde823cc4e4
Александр Федоткин <ben115899@gmail.com>
2024-06-02 20:23:48 +0700

    Создал пустой проект

---
+++ HW7/HW7/Modules/ListOfImage/View/ListOfImageViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ListOfImageViewController.swift
+//  HW7
+//
+//  Created by Александр Федоткин on 02.06.2024.
+//
+
+import UIKit
+
+class ListOfImageViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
