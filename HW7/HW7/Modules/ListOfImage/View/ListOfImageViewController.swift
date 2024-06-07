4af4b7246edd0b70292037eccbde15319de60f91
Александр Федоткин <ben115899@gmail.com>
2024-06-08 00:39:59 +0700

    Изменил методы + добавил скрытие клавиатуры

--- HW7/HW7/Modules/ListOfImage/View/ListOfImageViewController.swift
+++ HW7/HW7/Modules/ListOfImage/View/ListOfImageViewController.swift
@@ -15,7 +15,8 @@ class ListOfImageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-        setupClousure()
+        setupFindImageMethod()
+        setupKeyboardFunc()
         presenter.viewLoaded(ui: self)
     }
     
@@ -36,15 +37,24 @@ private extension ListOfImageViewController {
         view = componentView
     }
     
-    func setupClousure() {
+    func setupFindImageMethod() {
         componentView.findImage = { [weak self] text in
             self?.presenter.findImageFrom(text: text)
         }
     }
+    
+    func setupKeyboardFunc() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
 }
 
 extension ListOfImageViewController: ListOfImageDelegate, IListOfImageView {
-    func setupDataSource(dataSource: IListOfImageDataSource) {
+    func setupDataSource(_ dataSource: IListOfImageDataSource) {
         componentView.setupDataSource(dataSource: dataSource)
     }
     
