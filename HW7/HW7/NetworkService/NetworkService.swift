b985ac9fa4e4482ff4383e42b6844b5ef5359dde
Александр Федоткин <ben115899@gmail.com>
2024-06-08 00:40:35 +0700

    Минорные изменения

--- HW7/HW7/NetworkService/NetworkService.swift
+++ HW7/HW7/NetworkService/NetworkService.swift
@@ -25,7 +25,7 @@ enum NetworkService {
         let configuration = URLSessionConfiguration.background(withIdentifier: sessionID)
         configuration.isDiscretionary = true
         configuration.sessionSendsLaunchEvents = true
-        return URLSession(configuration: configuration, delegate: URLSessionDelegateImplement.shared, delegateQueue: nil)
+        return URLSession(configuration: configuration, delegate: BackgroundSessionConfigurator.shared, delegateQueue: nil)
     }
 }
 
@@ -36,7 +36,7 @@ extension NetworkService {
         case .getURLRequest(let parameters):
             var baseComponent = URLComponents(string: "https://www.themealdb.com/api/json/v1/1/search.php")
             let queryParameters = parameters.map {
-                URLQueryItem(name: $0.key, value: ($0.value as! String))
+                URLQueryItem(name: $0.key, value: ($0.value as? String))
             }
             baseComponent?.queryItems = queryParameters
             
@@ -58,60 +58,13 @@ extension NetworkService {
     
     func fetch(completion: @escaping (Result<Data, Error>) -> Void) {
         guard let request = makeRequest() else {
-            fatalError(Errors.badURL.rawValue)
+            completion(.failure(Errors.badURL.errorDescription))
+            return
         }
         
         let task = urlSession.dataTask(with: request)
         task.taskDescription = UUID().uuidString
-        URLSessionDelegateImplement.shared.setCompletionHandler(for: task, handler: completion)
+        BackgroundSessionConfigurator.shared.setCompletionHandler(for: task, handler: completion)
         task.resume()
     }
 }
-
-class URLSessionDelegateImplement: NSObject, URLSessionDelegate, URLSessionTaskDelegate, URLSessionDataDelegate {
-    static let shared = URLSessionDelegateImplement()
-    
-    var completionHandlers: [String: (Result<Data, Error>) -> Void] = [:]
-    var receivedData: [String: Data] = [:]
-
-    func urlSession(_ session: URLSession, didBecomeInvalidWithError error: Error?) {}
-    
-    func urlSessionDidFinishEvents(forBackgroundURLSession session: URLSession) {
-        DispatchQueue.main.async {
-            if let appDelegate = UIApplication.shared.delegate as? AppDelegate, let completionHandler = appDelegate.backgroundCompletion {
-                appDelegate.backgroundCompletion = nil
-                completionHandler()
-            }
-        }
-    }
-    
-    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
-        let taskDescription = task.taskDescription ?? ""
-        if let error = error {
-            if let completionHandler = completionHandlers[taskDescription] {
-                completionHandler(.failure(error))
-                completionHandlers.removeValue(forKey: taskDescription)
-            }
-        } else if let data = receivedData[taskDescription] {
-            if let completionHandler = completionHandlers[taskDescription] {
-                completionHandler(.success(data))
-                completionHandlers.removeValue(forKey: taskDescription)
-                receivedData.removeValue(forKey: taskDescription)
-            }
-        }
-        receivedData.removeValue(forKey: taskDescription)
-    }
-    
-    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
-        let taskDescription = dataTask.taskDescription ?? ""
-        if receivedData[taskDescription] == nil {
-            receivedData[taskDescription] = Data()
-        }
-        receivedData[taskDescription]?.append(data)
-    }
-    
-    func setCompletionHandler(for task: URLSessionTask, handler: @escaping (Result<Data, Error>) -> Void) {
-        completionHandlers[task.taskDescription ?? ""] = handler
-    }
-}
-
